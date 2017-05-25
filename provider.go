cb496c0d08048f19b9aa3ef7b514520ef34a6731
Peter Bale <peter.bale@sky.uk>
2017-05-25 14:52:26 +0100

    Updated provider to allow re-allocation of ip addresses and simplifed client

--- provider.go
+++ provider.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-  "log"
+	"fmt"
+	"log"
 
-  "github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/peterbale/go-phpipam"
 )
 
+// Client struct for all Terraform provider methods
+type Client struct {
+	PhpIPAMClient *phpipam.Client
+}
+
+// Provider method to define all user inputs
 func Provider() *schema.Provider {
-  return &schema.Provider{
-    Schema: map[string]*schema.Schema{
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
 			"server_url": {
 				Type:        schema.TypeString,
 				Required:    true,
@@ -19,31 +27,45 @@ func Provider() *schema.Provider {
 				Type:        schema.TypeString,
 				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("PHPIPAM_USERNAME", nil),
-				Description: "phpIPAM Username",
+				Description: "Username",
 			},
-      "password": {
+			"password": {
 				Type:        schema.TypeString,
 				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("PHPIPAM_PASSWORD", nil),
-				Description: "phpIPAM Password",
+				Description: "Password",
+			},
+			"ssl_skip_verify": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("PHPIPAM_SSL_SKIP_VERIFY", nil),
+				Description: "Skip SSL verify check",
 			},
 		},
 
-    ResourcesMap: map[string]*schema.Resource{
-      "phpipam_address": resourcePhpIPAMAddress(),
-    },
+		ResourcesMap: map[string]*schema.Resource{
+			"phpipam_address": resourcePhpIPAMAddress(),
+		},
 
-    ConfigureFunc: providerConfigure,
-  }
+		ConfigureFunc: providerConfigure,
+	}
 }
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
-  config := Config{
-    ServerUrl:  d.Get("server_url").(string),
-    Username:   d.Get("username").(string),
-    Password:   d.Get("password").(string),
-  }
-
-  log.Println("[INFO] Initializing phpIPAM client")
-  return config.Client()
+	config := &phpipam.Config{
+		Hostname:      d.Get("server_url").(string),
+		Username:      d.Get("username").(string),
+		Password:      d.Get("password").(string),
+		SSLSkipVerify: d.Get("ssl_skip_verify").(bool),
+		Application:   "terraform",
+	}
+	phpIPAMClient, err := config.NewClient()
+	if err != nil {
+		return nil, fmt.Errorf("Error setting up phpIPAM client: %s", err)
+	}
+	log.Printf("[INFO] phpIPAM Client configured for server %s", config.Hostname)
+	client := &Client{
+		PhpIPAMClient: phpIPAMClient,
+	}
+	return client, nil
 }
--- resource_address.go
+++ resource_address.go
@@ -1,322 +1,329 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "strconv"
-  "errors"
-  "sync"
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"sync"
 
-  "github.com/hashicorp/terraform/helper/schema"
-  "github.com/peterbale/go-phpipam"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/peterbale/go-phpipam"
 )
 
 var addonLock sync.Mutex
 
+// AddressInformation struct to define ipaddress data
 type AddressInformation struct {
-  Hostname  string
-  Ip        string
-  Section   string
-  Subnet    string
-  Broadcast string
-  Gateway   string
-  BitMask   string
+	Hostname  string
+	IP        string
+	Section   string
+	Subnet    string
+	Broadcast string
+	Gateway   string
+	BitMask   string
 }
 
 func resourcePhpIPAMAddress() *schema.Resource {
-  return &schema.Resource{
-    Create: resourcePhpIPAMAddressCreate,
-    Read:   resourcePhpIPAMAddressRead,
-    Update: resourcePhpIPAMAddressrUpdate,
-    Delete: resourcePhpIPAMAddressDelete,
+	return &schema.Resource{
+		Create: resourcePhpIPAMAddressCreate,
+		Read:   resourcePhpIPAMAddressRead,
+		Update: resourcePhpIPAMAddressrUpdate,
+		Delete: resourcePhpIPAMAddressDelete,
 
-    Schema: map[string]*schema.Schema{
-      "hostname": &schema.Schema{
-        Type:     schema.TypeString,
-        Required: true,
-      },
-      "section": &schema.Schema{
-        Type:     schema.TypeString,
-        Required: true,
-      },
-      "subnet": &schema.Schema{
-        Type:     schema.TypeString,
-        Required: true,
-      },
-      "ip_address": &schema.Schema{
-        Type:     schema.TypeString,
-        Computed: true,
-      },
-      "broadcast": &schema.Schema{
-        Type:     schema.TypeString,
-        Computed: true,
-      },
-      "gateway": &schema.Schema{
-        Type:     schema.TypeString,
-        Computed: true,
-      },
-      "bitmask": &schema.Schema{
-        Type:     schema.TypeString,
-        Computed: true,
-      },
-    },
-  }
+		Schema: map[string]*schema.Schema{
+			"hostname": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"section": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"subnet": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"ip_address": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"broadcast": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"gateway": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"bitmask": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
 }
 
-func findSectionId(client *phpipam.Client, section string) (string, error) {
-  var sectionId string
-  sections, err := client.GetSections()
-  if err!=nil{
-    return sectionId, err
-  }
-  for _,element := range sections.Data {
-    if element.Name == section {
-      sectionId = element.Id
-    }
-  }
-  if len(sectionId) == 0 {
-    return sectionId, errors.New("Section Not Found")
-  }
-  return sectionId, nil
+func (c *Client) findSectionID(section string) (string, error) {
+	var sectionID string
+	sections, err := c.PhpIPAMClient.GetSections()
+	if err != nil {
+		return sectionID, err
+	}
+	for _, element := range sections.Data {
+		if element.Name == section {
+			sectionID = element.ID
+		}
+	}
+	if len(sectionID) == 0 {
+		return sectionID, errors.New("Section Not Found")
+	}
+	return sectionID, nil
 }
 
-func findSubnetId(client *phpipam.Client, sectionId string, subnet string) (string, error) {
-  var subnetId string
-  subnets, err := client.GetSectionsSubnets(sectionId)
-  if err!=nil{
-    return subnetId, err
-  }
-  for _,element := range subnets.Data {
-    if element.Description == subnet {
-      subnetId = element.Id
-    }
-  }
-  if len(subnetId) == 0 {
-    return subnetId, errors.New("Subnet Not Found")
-  }
-  return subnetId, nil
+func (c *Client) findSubnetID(sectionID string, subnet string) (string, error) {
+	var subnetID string
+	subnets, err := c.PhpIPAMClient.GetSectionsSubnets(sectionID)
+	if err != nil {
+		return subnetID, err
+	}
+	for _, element := range subnets.Data {
+		if element.Description == subnet {
+			subnetID = element.ID
+		}
+	}
+	if len(subnetID) == 0 {
+		return subnetID, errors.New("Subnet Not Found")
+	}
+	return subnetID, nil
 }
 
-func findExistingAddress(client *phpipam.Client, hostname string) (phpipam.AddressSearch, int, error) {
-  var totalFoundAddresses int
-  addresses, err := client.GetAddressSearch(hostname)
-  if err!=nil{
-    return addresses, totalFoundAddresses, err
-  }
-  totalFoundAddresses = len(addresses.Data)
-  return addresses, totalFoundAddresses, nil
+func (c *Client) findExistingAddress(hostname string) (phpipam.AddressSearch, int, error) {
+	var totalFoundAddresses int
+	addresses, err := c.PhpIPAMClient.GetAddressSearch(hostname)
+	if err != nil {
+		return addresses, totalFoundAddresses, err
+	}
+	totalFoundAddresses = len(addresses.Data)
+	return addresses, totalFoundAddresses, nil
 }
 
-func getAddressId(client *phpipam.Client, address string) (string, error) {
-  var addressId string
-  addressSearchIp, err := client.GetAddressSearchIp(address)
-  if err != nil {
-    return addressId, err
-  }
-  if len(addressSearchIp.Data) != 1 {
-    return addressId, errors.New("Address Over Allocated")
-  }
-  return addressSearchIp.Data[0].Id, nil
+func (c *Client) getAddressID(address string) (string, error) {
+	var addressID string
+	addressSearchIP, err := c.PhpIPAMClient.GetAddressSearchIP(address)
+	if err != nil {
+		return addressID, err
+	}
+	if len(addressSearchIP.Data) != 1 {
+		return addressID, errors.New("Address Over Allocated")
+	}
+	return addressSearchIP.Data[0].ID, nil
 }
 
-func getAddressInformation(client *phpipam.Client, addressId string) (*AddressInformation, error) {
-  var hostname, subnetId, subnet, sectionId, section, address, broadcast, gateway, bitmask string
-  addressData, err := client.GetAddress(addressId)
-  if err!=nil{
-    return nil, err
-  }
-  if addressData.Code == 200 {
-    hostname  = addressData.Data.Hostname
-    subnetId  = addressData.Data.SubnetId
-    address   = addressData.Data.Ip
-  } else {
-    return nil, nil
-  }
-  subnetData, err := client.GetSubnet(subnetId)
-  if err!=nil{
-    return nil, err
-  }
-  if subnetData.Code == 200 {
-    subnet    = subnetData.Data.Description
-    sectionId = subnetData.Data.SectionId
-    broadcast = subnetData.Data.Calculation.Broadcast
-    gateway   = subnetData.Data.Gateway.IPAddress
-    bitmask   = subnetData.Data.Calculation.BitMask
-  } else {
-    return nil, errors.New("Address Subnet Not Found")
-  }
-  sectionData, err := client.GetSection(sectionId)
-  if err!=nil{
-    return nil, err
-  }
-  if sectionData.Code == 200 {
-    section    = sectionData.Data.Name
-  } else {
-    return nil, errors.New("Subnet Section Not Found")
-  }
-  return &AddressInformation{
-    Hostname:   hostname,
-    Ip:         address,
-    Section:    section,
-    Subnet:     subnet,
-    Broadcast:  broadcast,
-    Gateway:    gateway,
-    BitMask:    bitmask,
-  }, nil
+func (c *Client) getAddressInformation(addressID string) (*AddressInformation, error) {
+	addressInformation := new(AddressInformation)
+	var subnetID, sectionID string
+	addressData, err := c.PhpIPAMClient.GetAddress(addressID)
+	if err != nil {
+		return nil, err
+	}
+	if addressData.Code == 200 {
+		addressInformation.Hostname = addressData.Data.Hostname
+		addressInformation.IP = addressData.Data.IP
+		subnetID = addressData.Data.SubnetID
+	} else {
+		return nil, nil
+	}
+	subnetData, err := c.PhpIPAMClient.GetSubnet(subnetID)
+	if err != nil {
+		return nil, err
+	}
+	if subnetData.Code == 200 {
+		addressInformation.Subnet = subnetData.Data.Description
+		addressInformation.Broadcast = subnetData.Data.Calculation.Broadcast
+		addressInformation.Gateway = subnetData.Data.Gateway.IPAddress
+		addressInformation.BitMask = subnetData.Data.Calculation.BitMask
+		sectionID = subnetData.Data.SectionID
+	} else {
+		return nil, errors.New("Address Subnet Not Found")
+	}
+	sectionData, err := c.PhpIPAMClient.GetSection(sectionID)
+	if err != nil {
+		return nil, err
+	}
+	if sectionData.Code == 200 {
+		addressInformation.Section = sectionData.Data.Name
+	} else {
+		return nil, errors.New("Subnet Section Not Found")
+	}
+	return addressInformation, nil
 }
 
-func checkAddressLive(client *phpipam.Client, addressId string) (int, error) {
-  var pingStatusBool int
-  pingStatus, err := client.GetAddressPing(addressId)
-  if err!=nil{
-    return pingStatusBool, err
-  }
-  if pingStatus.Code == 200 {
-    pingStatusBool = 1
-  } else {
-    pingStatusBool = 0
-  }
-  return pingStatusBool, nil
+func (c *Client) checkAddressLive(addressID string) (int, error) {
+	var pingStatusBool int
+	pingStatus, err := c.PhpIPAMClient.GetAddressPing(addressID)
+	if err != nil {
+		return pingStatusBool, err
+	}
+	if pingStatus.Code == 200 {
+		pingStatusBool = 1
+	} else {
+		pingStatusBool = 0
+	}
+	return pingStatusBool, nil
 }
 
-func checkAddressSubnet(existingSubnetId string, subnetId string) (int) {
-  var subnetMatchBool int
-  if existingSubnetId == subnetId {
-    subnetMatchBool = 1
-  } else {
-    subnetMatchBool = 0
-  }
-  return subnetMatchBool
+func checkAddressSubnet(existingSubnetID string, subnetID string) int {
+	var subnetMatchBool int
+	if existingSubnetID == subnetID {
+		subnetMatchBool = 1
+	} else {
+		subnetMatchBool = 0
+	}
+	return subnetMatchBool
 }
 
-func allocateNewAddress(client *phpipam.Client, subnetId string, hostname string) (phpipam.AddressFirstFree, error) {
-  newAddress, err := client.CreateAddressFirstFree(subnetId, hostname, "terraform")
-  if err!=nil{
-    return newAddress, err
-  }
-  return newAddress, nil
+func (c *Client) allocateNewAddress(subnetID string, hostname string) (phpipam.AddressFirstFree, error) {
+	newAddress, err := c.PhpIPAMClient.CreateAddressFirstFree(subnetID, hostname, "terraform")
+	if err != nil {
+		return newAddress, err
+	}
+	return newAddress, nil
 }
 
-func deleteExistingAddress(client *phpipam.Client, addressId string) (phpipam.AddressDelete, error) {
-  deleteAddress, err := client.DeleteAddress(addressId)
-  if err!=nil{
-    return deleteAddress, err
-  }
-  return deleteAddress, nil
+func (c *Client) deleteExistingAddress(addressID string) (phpipam.AddressDelete, error) {
+	deleteAddress, err := c.PhpIPAMClient.DeleteAddress(addressID)
+	if err != nil {
+		return deleteAddress, err
+	}
+	return deleteAddress, nil
 }
 
-func create(client *phpipam.Client, section string, subnet string, hostname string, update bool) (string, error) {
-  addonLock.Lock()
-  defer addonLock.Unlock()
+func (c *Client) create(section string, subnet string, hostname string, update bool) (string, error) {
+	addonLock.Lock()
+	defer addonLock.Unlock()
 
-  var addressId string
-  var err error
-  sectionId, err := findSectionId(client, section)
-  if err != nil {
-    return addressId, fmt.Errorf("Error Getting Section ID: %s", err)
-  }
-  subnetId, err := findSubnetId(client, sectionId, subnet)
-  if err != nil {
-    return addressId, fmt.Errorf("Error Getting Subnet ID: %s", err)
-  }
-  _, totalFoundAddresses, err := findExistingAddress(client, hostname)
-  if err != nil {
-    return addressId, fmt.Errorf("Error Finding Existing Addresses: %s", err)
-  }
-  if totalFoundAddresses == 0 || (totalFoundAddresses == 1 && update) {
-    log.Printf("[DEBUG] New Address Section ID: %#v, Subnet ID: %#v", sectionId, subnetId)
-    newAddress, err := allocateNewAddress(client, subnetId,hostname)
-    if err != nil {
-      return addressId, fmt.Errorf("Error Allocating New Address: %s", err)
-    }
-    log.Printf("[DEBUG] New Address IP: %#v", newAddress)
-    addressId, err = getAddressId(client, newAddress.Ip)
-    if err != nil {
-      return addressId, fmt.Errorf("Error Getting Created Address ID: %s", newAddress.Ip)
-    }
-    log.Printf("[INFO] New Address Allocated: %s", newAddress.Ip)
-  } else {
-    return addressId, fmt.Errorf("Error Address Already Allocated, Total Found Addresses: %s", strconv.Itoa(totalFoundAddresses))
-  }
-  return addressId, nil
+	var addressID string
+	sectionID, err := c.findSectionID(section)
+	if err != nil {
+		return addressID, fmt.Errorf("Error Getting Section ID: %s", err)
+	}
+	subnetID, err := c.findSubnetID(sectionID, subnet)
+	if err != nil {
+		return addressID, fmt.Errorf("Error Getting Subnet ID: %s", err)
+	}
+	addresses, totalFoundAddresses, err := c.findExistingAddress(hostname)
+	if err != nil {
+		return addressID, fmt.Errorf("Error Finding Existing Addresses: %s", err)
+	}
+	if totalFoundAddresses == 0 || (totalFoundAddresses == 1 && update) {
+		log.Printf("[DEBUG] New Address Section ID: %#v, Subnet ID: %#v", sectionID, subnetID)
+		newAddress, err := c.allocateNewAddress(subnetID, hostname)
+		if err != nil {
+			return addressID, fmt.Errorf("Error Allocating New Address: %s", err)
+		}
+		log.Printf("[DEBUG] New Address IP: %#v", newAddress)
+		addressID, err = c.getAddressID(newAddress.IP)
+		if err != nil {
+			return addressID, fmt.Errorf("Error Getting Created Address ID: %s", newAddress.IP)
+		}
+		log.Printf("[INFO] New Address Allocated: %s", newAddress.IP)
+	} else if totalFoundAddresses == 1 && !update {
+		log.Printf("[DEBUG] Existing Address Section ID: %#v, Subnet ID: %#v", sectionID, subnetID)
+		log.Printf("[DEBUG] Existing Address IP: %#v", addresses.Data[0].IP)
+		addressID, err = c.getAddressID(addresses.Data[0].IP)
+		if err != nil {
+			return addressID, fmt.Errorf("Error Getting Created Address ID: %s", addresses.Data[0])
+		}
+		addressState, err := c.checkAddressLive(addressID)
+		if err != nil {
+			return addressID, fmt.Errorf("Address Liveliness Check Failed: %s", err)
+		} else if addressState != 0 {
+			return addressID, fmt.Errorf("Existing Address Host is Still Live")
+		}
+		log.Printf("[INFO] New Address Allocated: %s", addresses.Data[0].IP)
+	} else {
+		return addressID, fmt.Errorf("Error Address Already Allocated, Total Found Addresses: %s", strconv.Itoa(totalFoundAddresses))
+	}
+	return addressID, nil
 }
 
-func delete(client *phpipam.Client, addressId string, update bool) error {
-  if ! update {
-    addressState, err := checkAddressLive(client, addressId)
-    if err != nil {
-      return fmt.Errorf("Address Liveliness Check Failed: %s", err)
-    } else if addressState != 0 {
-      return fmt.Errorf("Address Host is Still Live")
-    }
-  }
-  _, err := deleteExistingAddress(client, addressId)
-  if err != nil {
-    return fmt.Errorf("Delete Address Failed: %s", err)
-  }
-  log.Printf("[INFO] Address Removed: %s", addressId)
-  return nil
+func (c *Client) delete(addressID string, update bool) error {
+	if !update {
+		addressState, err := c.checkAddressLive(addressID)
+		if err != nil {
+			return fmt.Errorf("Address Liveliness Check Failed: %s", err)
+		} else if addressState != 0 {
+			return fmt.Errorf("Address Host is Still Live")
+		}
+	}
+	_, err := c.deleteExistingAddress(addressID)
+	if err != nil {
+		return fmt.Errorf("Delete Address Failed: %s", err)
+	}
+	log.Printf("[INFO] Address Removed: %s", addressID)
+	return nil
 }
 
 func resourcePhpIPAMAddressCreate(d *schema.ResourceData, m interface{}) error {
-  section := d.Get("section").(string)
-  subnet := d.Get("subnet").(string)
-  hostname := d.Get("hostname").(string)
-  client := m.(*phpipam.Client)
-  addressId, err := create(client, section, subnet, hostname, false)
-  if err != nil {
-    return err
-  }
-  d.SetId(addressId)
-  return resourcePhpIPAMAddressRead(d, m)
+	section := d.Get("section").(string)
+	subnet := d.Get("subnet").(string)
+	hostname := d.Get("hostname").(string)
+	client := m.(*Client)
+	addressID, err := client.create(section, subnet, hostname, false)
+	if err != nil {
+		return err
+	}
+	d.SetId(addressID)
+	return resourcePhpIPAMAddressRead(d, m)
 }
 
 func resourcePhpIPAMAddressRead(d *schema.ResourceData, m interface{}) error {
-  client := m.(*phpipam.Client)
-  log.Printf("[INFO] Address ID Created: %s", d.Id())
-  addressInformation, err := getAddressInformation(client, d.Id())
-  if err!=nil{
-    return fmt.Errorf("Cannot Get Address Infomation: %s", err)
-  }
-  d.Set("hostname", addressInformation.Hostname)
-  d.Set("section", addressInformation.Section)
-  d.Set("subnet", addressInformation.Subnet)
-  d.Set("ip_address", addressInformation.Ip)
-  d.Set("broadcast", addressInformation.Broadcast)
-  d.Set("gateway", addressInformation.Gateway)
-  d.Set("bitmask", addressInformation.BitMask)
-  return nil
+	client := m.(*Client)
+	log.Printf("[INFO] Address ID Created: %s", d.Id())
+	addressInformation, err := client.getAddressInformation(d.Id())
+	if err != nil {
+		return fmt.Errorf("Cannot Get Address Infomation: %s", err)
+	}
+	d.Set("hostname", addressInformation.Hostname)
+	d.Set("section", addressInformation.Section)
+	d.Set("subnet", addressInformation.Subnet)
+	d.Set("ip_address", addressInformation.IP)
+	d.Set("broadcast", addressInformation.Broadcast)
+	d.Set("gateway", addressInformation.Gateway)
+	d.Set("bitmask", addressInformation.BitMask)
+	return nil
 }
 
 func resourcePhpIPAMAddressrUpdate(d *schema.ResourceData, m interface{}) error {
-  section := d.Get("section").(string)
-  subnet := d.Get("subnet").(string)
-  hostname := d.Get("hostname").(string)
-  client := m.(*phpipam.Client)
-  addressId := d.Id()
-  var err error
-  if d.HasChange("hostname") {
-    _, err = client.PatchUpdateAddress(hostname, addressId,)
-    if err != nil {
-      return fmt.Errorf("Address Update Failed: %s", err)
-    }
-    log.Printf("[INFO] Address Updated: %s", hostname)
-  } else {
-    newAddressId, err := create(client, section, subnet, hostname, true)
-    if err != nil {
-      return err
-    }
-    err = delete(client, addressId, true)
-    if err != nil {
-      return err
-    }
-    d.SetId(newAddressId)
-  }
-  return resourcePhpIPAMAddressRead(d, m)
+	section := d.Get("section").(string)
+	subnet := d.Get("subnet").(string)
+	hostname := d.Get("hostname").(string)
+	client := m.(*Client)
+	addressID := d.Id()
+	var err error
+	if d.HasChange("hostname") {
+		_, err = client.PhpIPAMClient.PatchUpdateAddress(hostname, addressID)
+		if err != nil {
+			return fmt.Errorf("Address Update Failed: %s", err)
+		}
+		log.Printf("[INFO] Address Updated: %s", hostname)
+	} else {
+		newAddressID, err := client.create(section, subnet, hostname, true)
+		if err != nil {
+			return err
+		}
+		err = client.delete(addressID, true)
+		if err != nil {
+			return err
+		}
+		d.SetId(newAddressID)
+	}
+	return resourcePhpIPAMAddressRead(d, m)
 }
 
 func resourcePhpIPAMAddressDelete(d *schema.ResourceData, m interface{}) error {
-  client := m.(*phpipam.Client)
-  err := delete(client, d.Id(), false)
-  return err
+	client := m.(*Client)
+	err := client.delete(d.Id(), false)
+	return err
 }
